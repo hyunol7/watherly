8103f61af4be810fb502ac1ca5ecfb1641eabb7e
juhyun517 <cjuhyun0517@naver.com>
2025-04-28 14:01:12 +0900

    memo dto

---
+++ src/main/java/com/weatherly/domain/Memo.java
@@ -0,0 +1,16 @@
+package com.weatherly.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    private int id;
+    private String text;
+
+}
